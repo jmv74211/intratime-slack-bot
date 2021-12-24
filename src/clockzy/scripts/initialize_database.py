cfa9defb66e80af8f26e06149c97ba6d73b27852
jmv74211 <jonathan.martin@wazuh.com>
2021-12-24 18:09:15 +0100

    refac: Update script to create and initialize the DB

--- src/clockzy/scripts/initialize_database.py
+++ src/clockzy/scripts/initialize_database.py
@@ -6,8 +6,8 @@ from clockzy.lib.db.database import Database
 from clockzy.config.settings import DB_NAME
 from clockzy.lib.db import db_schema as dbs
 
-TABLES = [dbs.USER_TABLE, dbs.CLOCK_TABLE, dbs.COMMANDS_HISTORY_TABLE, dbs.CONFIG_TABLE, dbs.ALIAS_TABLE]
-
+SCHEMAS = [dbs.USER_TABLE_SCHEMA, dbs.CLOCK_TABLE_SCHEMA, dbs.COMMANDS_HISTORY_TABLE_SCHEMA, dbs.CONFIG_TABLE_SCHEMA,
+           dbs.ALIAS_TABLE_SCHEMA]
 
 if __name__ == '__main__':
     database = Database()
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     database.create_database(DB_NAME)
 
     # Create the tables if not exist
-    for table in TABLES:
-        database.run_query(table)
+    for table_schema in SCHEMAS:
+        database.run_query(table_schema)
 
     database.close()
